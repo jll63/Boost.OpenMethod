e1658db6d1e87a63a8f83e1568a752dd5de67074
Jean-Louis Leroy <jl@leroy.nyc>
2025-06-19 17:42:55 -0400

    remove unnecessary reinterpret_cast

--- include/boost/openmethod/policies/minimal_rtti.hpp
+++ include/boost/openmethod/policies/minimal_rtti.hpp
@@ -17,7 +17,7 @@ struct minimal_rtti : virtual rtti {
     template<typename T>
     static auto static_type() -> type_id {
         static char id;
-        return reinterpret_cast<type_id>(&id);
+        return &id;
     }
 };
 

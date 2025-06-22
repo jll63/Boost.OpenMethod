83c8ab9afd9f68bfb7be8af1a7448044df56bee6
Jean-Louis Leroy <jl@leroy.nyc>
2025-06-22 11:05:19 -0400

    CI: adopt boost-cli GHA

---
+++ test/compile_fail_not_polymorphic_virtual_ptr.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2018-2025 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/openmethod.hpp>
+
+struct Cat {};
+
+Cat felix;
+boost::openmethod::virtual_ptr<Cat> p(felix);
